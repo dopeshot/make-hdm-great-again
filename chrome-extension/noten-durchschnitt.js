1eb3a035dce38f1a8b7339e275162bd63dc8d6d1
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-05-16 17:17:47 +0200

    Add state variables and input fields

--- chrome-extension/noten-durchschnitt.js
+++ chrome-extension/noten-durchschnitt.js
@@ -1,14 +1,14 @@
+const list = document.createElement("p")
 const headline = document.querySelector("h1")
+const table = document.querySelectorAll('table')[1]
+// State
+let averageGrades = 0
+let GrundstudiumWeight = 15
+let HauptstudiumWeight = 70
+let BachelorWeight = 15
 
-function main() {
-    // Safety-check
-    if (headline.textContent !== "Notenspiegel") {
-        console.warn("Seems like you are not on Notenspiegel.")
-        return
-    }
-
+function getGrades() {
     // Get all grades
-    const table = document.querySelectorAll('table')[1]
     let kind = null
     const grades = [...table.querySelectorAll('tr')].reduce((accumulator, row) => {
         const firstThRow = row.querySelector('th')
@@ -52,45 +52,95 @@ function main() {
 
     }, { GrundstudiumNote: 0.0, GrundstudiumECTS: 0, HauptstudiumNote: 0.0, HauptstudiumECTS: 0 })
 
+    console.log(sumOfGrades)
+
     const averageGrades = {
         GrundstudiumAverage: sumOfGrades.GrundstudiumNote / sumOfGrades.GrundstudiumECTS,
         hauptstudiumAverage: sumOfGrades.HauptstudiumNote / sumOfGrades.HauptstudiumECTS,
         BachelorAverage: (sumOfGrades.GrundstudiumNote + sumOfGrades.HauptstudiumNote) / (sumOfGrades.GrundstudiumECTS + sumOfGrades.HauptstudiumECTS)
     }
 
-    const GrundstudiumWeight = 0.15
-    const HauptstudiumWeight = 0.70
-    const BachelorWeight = 0.15
+    return averageGrades
+}
 
+function makePrediction(averageGrades) {
     const prediction = {
-        GradePrediction: averageGrades.GrundstudiumAverage * GrundstudiumWeight + averageGrades.hauptstudiumAverage * HauptstudiumWeight + averageGrades.BachelorAverage * BachelorWeight,
+        GradePrediction: averageGrades.GrundstudiumAverage * GrundstudiumWeight/100 + averageGrades.hauptstudiumAverage * HauptstudiumWeight/100 + averageGrades.BachelorAverage * BachelorWeight/100,
     }
 
-    console.log(sumOfGrades)
-    console.log(averageGrades)
-    console.log(prediction)
+    return prediction
+}
 
+function updatePrediction(prediction) {
+    // Create Grade prediction text
+    const result = `Gesamt Note: ${prediction.GradePrediction.toFixed(2)}\n`
+    list.textContent = result
+}
+
+function addHtml() {
     // Create container for information
     const container = document.createElement("div")
     container.style.backgroundColor = "#5381BE"
     container.style.color = "#fff"
 
-    // Create Grade prediction text
-    const list = document.createElement("p")
-    const result = `Gesamt Note: ${prediction.GradePrediction.toFixed(2)}\n`
-    list.textContent = result
-
     // Create input fields
-    const inputField = document.createElement("input")
-    inputField.
+    const grundstudiumSettings = document.createElement("p")
+    const grundstudiumInput = document.createElement("input")
+    grundstudiumInput.type = "number"
+    grundstudiumInput.value = GrundstudiumWeight
+    grundstudiumInput.style.alignContent = "right"
+    grundstudiumInput.style.width = "50px"
+    grundstudiumInput.addEventListener("change", function () { GrundstudiumWeight = grundstudiumInput.value; updatePrediction(makePrediction(averageGrades)) })
+    grundstudiumSettings.textContent = `Grundstudium Gewichtung: `
+    grundstudiumSettings.append(grundstudiumInput)
 
-    container.append(inputField)
+    const hauptstudiumSettings = document.createElement("p")
+    const hauptstudiumInput = document.createElement("input")
+    hauptstudiumInput.type = "number"
+    hauptstudiumInput.value = HauptstudiumWeight
+    hauptstudiumInput.style.alignContent = "right"
+    hauptstudiumInput.style.width = "50px"
+    hauptstudiumInput.addEventListener("change", function () { HauptstudiumWeight = hauptstudiumInput.value; updatePrediction(makePrediction(averageGrades)) })
+    hauptstudiumSettings.textContent = `Hauptstudium Gewichtung: `
+    hauptstudiumSettings.append(hauptstudiumInput)
+
+    const bachelorSettings = document.createElement("p")
+    const bachelorInput = document.createElement("input")
+    bachelorInput.type = "number"
+    bachelorInput.value = BachelorWeight
+    bachelorInput.style.alignContent = "right"
+    bachelorInput.style.width = "50px"
+    bachelorInput.addEventListener("change", function () { BachelorWeight = bachelorInput.value; updatePrediction(makePrediction(averageGrades)) })
+    bachelorSettings.textContent = `Bachelor Gewichtung: `
+    bachelorSettings.append(bachelorInput)
+
+    container.append(grundstudiumSettings)
+    container.append(hauptstudiumSettings)
+    container.append(bachelorSettings)
     container.append(list)
     insertAfter(container, table)
 
     function insertAfter(newNode, referenceNode) {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
     }
+}
+
+function main() {
+    // Safety-check
+    if (headline.textContent !== "Notenspiegel") {
+        console.warn("Extension does not work on this site, missing topic 'Notenspiegel'")
+        return
+    }
+
+    averageGrades = getGrades()
+    console.log(averageGrades)
+
+    const prediction = makePrediction(averageGrades)
+    console.log(prediction)
+
+    updatePrediction(prediction)
+
+    addHtml()
 }
 
 console.log("Make Hdm Great Again Loaded!")
